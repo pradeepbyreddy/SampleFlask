f21a72c7cce419486cbb4df0757a58b6d7027d23
pradeepbyreddy <byreddypradeep.sai@gmail.com>
2021-04-03 15:04:45 +0530

    Sample first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask("__name__")
+userList=['Pradeep','SecondUser', 'User3']
+@app.route('/')
+
+def index():
+    return "home page"
+
+@app.route('/profile/<profileName>')
+def username(profileName):
+    if profileName in userList:
+        return "<h3>Hello! %s<h3>" % profileName
+    else:
+        return "there are no users are added to this website named %s" % profileName
+
+@app.route('/id/<int:userID>')
+def id(userID):
+    return "<h2>userID is %s<h2>" % userID
+
+if __name__ == '__main__':
+    app.run(debug=True)
